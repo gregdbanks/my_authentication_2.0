268472d62e9da43068ed5a48b8ec5ba2d7c79605
Greg Banks <gregbanks@Gregs-MacBook-Pro.local>
2022-02-03 06:06:29 -0600

    Adding sign up route and necessary middleware

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const user = require("./routes/user"); //new addition
+const InitiateMongoServer = require("./config/db");
+
+// Initiate Mongo Server
+InitiateMongoServer();
 
 const app = express();
 
 // PORT
 const PORT = process.env.PORT || 4000;
 
+// Middleware
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "token, Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// Middleware
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.json({ message: "API Working" });
 });
+
+/**
+ * Router Middleware
+ * Router - /user/*
+ * Method - *
+ */
+app.use("/user", user);
 
 app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
